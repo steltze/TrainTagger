73f6f47ac50a2a5e37c301d96f1b825d6c6825ea
stzelepi <stylianos.tzelepis@cern.ch>
2025-03-07 15:57:26 +0100

    Example with padded data and masking

--- PID/test.py
+++ PID/test.py
@@ -3,6 +3,36 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from tagger.train.models import baseline
+import utils
+
+num_threads = 8
+os.environ["OMP_NUM_THREADS"] = str(num_threads)
+os.environ["TF_NUM_INTRAOP_THREADS"] = str(num_threads)
+os.environ["TF_NUM_INTEROP_THREADS"] = str(num_threads)
+
+tf.config.threading.set_inter_op_parallelism_threads(
+    num_threads
+)
+tf.config.threading.set_intra_op_parallelism_threads(
+    num_threads
+)
+
+# GLOBAL PARAMETERS TO BE DEFINED WHEN TRAINING
+tf.keras.utils.set_random_seed(420) #not a special number 
+BATCH_SIZE = 1024
+EPOCHS = 100
+VALIDATION_SPLIT = 0.1 # 10% of training set will be used for validation set. 
+
+# Sparsity parameters
+I_SPARSITY = 0.0 #Initial sparsity
+F_SPARSITY = 0.1 #Final sparsity
+
 def get_test_model(num_channels):
 
     # Input: A set of samples (None means arbitrary number of samples per set)
@@ -49,21 +79,42 @@ def generate_data(num_sets, max_samples, num_channels):
     
     return X, y
 
-def main():
-    num_channels = 10 
-    model = get_test_model(num_channels)
-
+def train_test_model():
+    num_channels = 10    
+    
     num_sets = 5000
     max_samples = 20
     num_channels = 10
 
     X_train, y_train = generate_data(num_sets, max_samples, num_channels)
+    
+    model = baseline((0, num_channels), (1, 1))
+    model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+
+
 
     model.fit(X_train, y_train, epochs=10, batch_size=32)
 
-    X_test, y_test = generate_data(1000, max_samples, num_channels)
+    X_test, y_test = generate_data(100, max_samples, num_channels)
 
     model.evaluate(X_test, y_test)
+    
+def train_qkeras_model():
+    num_channels = 10 
+    model = get_test_model(num_channels)
+
+    num_sets = 50000
+    max_samples = 20
+    num_channels = 10
+
+    X_train, y_train = generate_data(num_sets, max_samples, num_channels)
+    X_test, y_test = generate_data(1000, max_samples, num_channels)
+
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
+
+    model.evaluate(X_test, y_test)
+    
+    utils.plot_losses(history)
 
 if __name__ == "__main__":
-    main()
+    train_qkeras_model()
---
+++ PID/utils.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+
+def plot_losses(history):
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    # Plot the training and validation loss curves
+    plt.figure(figsize=(8, 6))
+    plt.plot(train_loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('training_loss_plot.png', dpi=300)  # Saves as a high-resolution PNG file
+    plt.show()
